592c52330d926aeae2fe1d1058d4743763ff80e8
RaoulBongarzoni <raoul.bongarzoni99@gmail.com>
2024-01-12 16:40:20 +0100

    directory change for 'require' statement

--- index.php
+++ index.php
@@ -1,8 +1,27 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-require __DIR__ . 'php-oop-1/Models/Movie.php';
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
 
-$mov_first = new Movie("test", "regista", "horror", 1999, "lorem ipsum", "immagine");
+<body>
 
-echo "<h2>stampa di test</h2>";
-var_dump($mov_first);
+
+    <h1>test</h1>
+
+    <?php
+
+    require __DIR__ . '/Models/Movie.php';
+
+    $mov_first = new Movie("test", "regista", "horror", 1999, "lorem ipsum", "immagine");
+
+    echo "<h2>stampa di test</h2>";
+    var_dump($mov_first);
+    ?>
+
+</body>
+
+</html>
